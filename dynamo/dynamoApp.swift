378c6c48e6695fb2216ddf768620f7e657d01d48
Laurynas Antanavičius <laurynas.ant@gmail.com>
2023-12-01 16:22:28 +0200

    Initial Commit

---
+++ dynamo/dynamoApp.swift
@@ -0,0 +1,17 @@
+//
+//  dynamoApp.swift
+//  dynamo
+//
+//  Created by Laurynas Antanavicius on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct dynamoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
